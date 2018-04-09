982040cb23f8288589964ff7b3ca5266beb86f7a
cs-dtalwar <dtalwar@cambriasolutions.com>
2018-04-09 12:16:34 -0700

    Update init-user-db.sh

--- docker/postgres/init-user-db.sh
+++ docker/postgres/init-user-db.sh
@@ -4,10 +4,10 @@ set -e
 
 psql postgres -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 
-    CREATE USER pguser WITH PASSWORD 'pguser';
+    CREATE USER ci_tester WITH PASSWORD 'pa33word';
 
-    CREATE DATABASE pguser;
+    CREATE DATABASE travis_ci_test;
 
-    GRANT ALL PRIVILEGES ON DATABASE pguser TO pguser;
+    GRANT ALL PRIVILEGES ON DATABASE travis_ci_test TO ci_tester;
 
 EOSQL
