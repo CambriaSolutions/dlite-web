0fbd785004991befb715d2f87d8746420120c4df
cs-dtalwar <dtalwar@cambriasolutions.com>
2018-04-12 22:50:05 -0700

    Update deploy.sh

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -4,3 +4,9 @@ docker rmi $(docker images -qf dangling=true --no-trunc) || true
 docker pull cambria/devpostgres:latest
 docker pull cambria/aliceredis:latest
 docker pull cambria/alice:latest
+docker tag cambria/devpostgres:latest devpostgres:latest
+docker tag cambria/aliceredis:latest aliceredis:latest
+docker tag cambria/alice:latest alice:latest
+cd $HOME/docker/postgres && ./stoppostgres && ./runpostgres
+cd $HOME/docker/redis && ./stopredis && ./runredis
+cd $HOME/docker/alice && ./stopalice && ./runalice
