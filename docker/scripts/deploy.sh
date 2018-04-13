c0e46502a34077e42f67dd4e58786bd760083899
cs-dtalwar <dtalwar@cambriasolutions.com>
2018-04-12 22:37:56 -0700

    Update deploy.sh

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -1,6 +1,6 @@
-docker rm \$(docker ps -q -f status=exited) || true
-docker volume rm \$(docker volume ls -qf dangling=true) || true
-docker rmi \$(docker images -qf dangling=true --no-trunc) || true
+docker rm $(docker ps -q -f status=exited) || true
+docker volume rm $(docker volume ls -qf dangling=true) || true
+docker rmi $(docker images -qf dangling=true --no-trunc) || true
 docker pull cambria/devpostgres:latest
 docker pull cambria/aliceredis:latest
 docker pull cambria/alice:latest
